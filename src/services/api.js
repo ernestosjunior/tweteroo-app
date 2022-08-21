5a7c0410cb25a16e9a8153308b6e4699c79de4e1
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-21 18:03:06 -0300

    feat: create api fuctions

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,21 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://tweteroo-api.herokuapp.com",
 });
+
+export const loadTweets = async () => {
+  const { data } = await api.get("/tweets");
+
+  return data;
+};
+
+export const postTweet = async ({ username, twwet }) => {
+  const { data } = await api.post("/tweets", { username, twwet });
+
+  return data;
+};
+
+export const signUpRequest = async ({ username, avatar }) => {
+  const { data } = await api.post("sign-up", { username, avatar });
+
+  return data;
+};
