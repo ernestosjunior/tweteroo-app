29c6ec7822b458363ea458ba839bc3fddb17a650
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-21 17:40:01 -0300

    feat: create signup page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,3 +1,20 @@
+import logo from "../../assets/logo.png";
+import styles from "./index.module.css";
+
 export const SignUp = () => {
-  return <>SignUp</>;
+  return (
+    <div class={styles.paginaInicial}>
+      <h1>tweteroo</h1>
+      <div class={styles.form}>
+        <img src={logo} alt="Tweteroo" />
+        <input type="text" placeholder="digite seu username" id="username" />
+        <input
+          type="text"
+          placeholder="e cole a url da sua linda foto"
+          id="picture"
+        />
+      </div>
+      <button onclick="signUp()">enviar</button>
+    </div>
+  );
 };
