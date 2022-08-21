c5d646837f291f71b5307cd5a28f07a6decd4150
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-21 18:09:14 -0300

    feat: create signup

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,15 +1,24 @@
 import { useState, createContext, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 import { signUpRequest } from "../services/api";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   const isLogged = !!user;
 
   const signUp = async ({ username, avatar }) => {
     const data = await signUpRequest({ username, avatar });
+
+    if (!data || !data.id) return toast.error("Credenciais inválidas!");
+
+    setUser(data);
+    toast.success("Bem-vindo!");
+    return navigate("/");
   };
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import App from "./App";
 import "./assets/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster position="top-center" />
     <App />
   </React.StrictMode>
 );
